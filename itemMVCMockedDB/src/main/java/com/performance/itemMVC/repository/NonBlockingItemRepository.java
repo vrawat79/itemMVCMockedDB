1b8f1e17fe025342deaf0ebe902bfce8431395ea
vikramrawat <vikramrawat@localhost>
2019-11-09 11:41:12 +0530

    solution for Lal's query

--- itemMVCMockedDB/src/main/java/com/performance/itemMVC/repository/NonBlockingItemRepository.java
+++ itemMVCMockedDB/src/main/java/com/performance/itemMVC/repository/NonBlockingItemRepository.java
@@ -1,6 +1,10 @@
 package com.performance.itemMVC.repository;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 import javax.annotation.PostConstruct;
 
@@ -32,8 +36,10 @@ public class NonBlockingItemRepository implements ItemRepository {
 		System.out.println("url: " + url);
 	}
 
+	Map<String, List<Item>> resultMap = new HashMap<String, List<Item>>();
+
 	@Override
-	public Flux<Item> findByCategory(String category) {		
+	public Flux<Item> findByCategory(String category) {
 		Flux<Item> itemFlux = null;
 		if (category.equalsIgnoreCase("catalogue")) {
 			return itemFlux = webClient.get().uri("/stream/" + category).exchange()
@@ -42,19 +48,32 @@ public class NonBlockingItemRepository implements ItemRepository {
 
 		itemFlux = webClient.get().uri("/" + category).exchange()
 				.flatMapMany(response -> response.bodyToFlux(Item.class));
+
+		String txid = UUID.randomUUID().toString();
+		resultMap.put(txid, new ArrayList<Item>());
+
+		// blocking implementation
+		itemFlux.collectList().map(itemList -> {
+			System.out.println("---- save to db: " + itemList);
+			return itemList;
+		}).subscribe();
+
+		// non-blocking implementation
+		itemFlux.map(item -> {
+			List<Item> itemList = resultMap.get(txid);
+			itemList.add(item);
+			return item;
+		}).doOnComplete(() -> {
+			System.out.println("-- publish an event with the list to an async consumer ---");
+			List<Item> itemList = resultMap.get(txid);
+
+			// save the itemList to DB or publish to bus for further logic
+
+			// remove the txid from the map
+			resultMap.remove(txid);
+		}).subscribe();
+
 		return itemFlux;
 	}
-
-//	public List<Item> findByCategory(String category) {
-//
-//		ResponseEntity<List<Item>> response = restTemplate.exchange(url, HttpMethod.GET, null,
-//				new ParameterizedTypeReference<List<Item>>() {
-//				});
-//		List<Item> itemList = response.getBody();
-//
-//		return itemList;
-//
-////		return webClient.get().uri("/").exchange().flatMapMany(response -> response.bodyToFlux(Item.class));
-//	}
 
 }
